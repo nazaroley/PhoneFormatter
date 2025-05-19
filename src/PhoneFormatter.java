7814f476ee12ae97ce368675f98cf1368e9677d3
nazaroley <Ponoexalca@gmail.com>
2025-05-20 02:04:12 +0300

    Добавлено чтение из файла

--- src/PhoneFormatter.java
+++ src/PhoneFormatter.java
@@ -1,5 +1,12 @@
+import java.io.*;
+
 public class PhoneFormatter {
-    public static void main(String[] args) {
-        System.out.println("Программа запущена");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
     }
 }
