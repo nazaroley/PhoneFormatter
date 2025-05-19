f05e7db0d42488194aa2a55658cfab4c0dd62ed9
nazaroley <Ponoexalca@gmail.com>
2025-05-20 02:23:25 +0300

    Добавлен базовый класс

--- src/PhoneFormatter.java
+++ src/PhoneFormatter.java
@@ -1,4 +1,5 @@
-BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-writer.write(line.replaceAll("(\\+7|8)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", 
-            "+1 ($2) $3-$4-$5"));
-writer.close();
+public class PhoneFormatter {
+    public static void main(String[] args) {
+        System.out.println("Программа запущена");
+    }
+}
