d6892582c22acb0d1df92f728f68e61aea370dec
nazaroley <Ponoexalca@gmail.com>
2025-05-20 02:11:40 +0300

    Добавлена запись в output.txt

--- src/PhoneFormatter.java
+++ src/PhoneFormatter.java
@@ -1,12 +1,4 @@
-import java.io.*;
-
-public class PhoneFormatter {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-        reader.close();
-    }
-}
+BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+writer.write(line.replaceAll("(\\+7|8)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", 
+            "+1 ($2) $3-$4-$5"));
+writer.close();
